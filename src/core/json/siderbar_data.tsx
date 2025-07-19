cc27aafbd3142e70f5f54e979638145b3ee09525
Siva1234588 <sivasaisiginam999@gmail.com>
2025-07-19 11:53:18 +0530

    Created daily tax report

--- src/core/json/siderbar_data.tsx
+++ src/core/json/siderbar_data.tsx
@@ -589,6 +589,13 @@ export const SidebarData = [
         showSubRoute: false,
         submenu: false,
       },
+       {
+        label: "Daily Tax Report ",
+        link: "/daily-tax-report",
+        icon: "tax",
+        showSubRoute: false,
+        submenu: false,
+      },
     ],
   },
 
