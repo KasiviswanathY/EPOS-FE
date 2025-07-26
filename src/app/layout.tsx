785a6851134bf3e8bd9f59e2775940736892a4f7
Siva1234588 <112367189+Siva1234588@users.noreply.github.com>
2025-07-26 23:33:57 +0530

    Revert "Login Integration"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,7 @@ import "../style/icons/tabler-icons/webfont/tabler-icons.css";
 import "../style/icons/fontawesome/css/fontawesome.min.css";
 import "../style/icons/fontawesome/css/all.min.css";
 import "../style/fonts/feather/css/iconfont.css";
-
 import BootstrapJs from "../components/bootstrap-js/bootstrapjs";
-import { Providers } from "./providers"; // ✅ Import the wrapper here
 
 export const metadata = {
   title: "Dreams POS - Inventory Management & Admin Dashboard Template",
@@ -19,8 +17,8 @@ export const metadata = {
   author: "Dreams Technologies",
   icons: {
     icon: "favicon.png",
-    shortcut: "favicon.png",
-    apple: "favicon.png",
+    shortcut: "favicon.png", // Add shortcut icon for better support
+    apple: "favicon.png", // Optional: for Apple devices (place in `public/`)
   },
 };
 
@@ -32,10 +30,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Providers>
-          {children}
+        <>
+          <>{children}</>
           <BootstrapJs />
-        </Providers>
+        </>
       </body>
     </html>
   );
