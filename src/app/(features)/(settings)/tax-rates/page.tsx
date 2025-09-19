9e1abb635dcf53585f362b9de7333c0eaa6d5055
Siva1234588 <sivasaisiginam999@gmail.com>
2025-09-19 15:43:14 +0530

    tax_companies

--- src/app/(features)/(settings)/tax-rates/page.tsx
+++
@@ -1,9 +0,0 @@
-import TaxRatesComponent from "@/components/settings/financialsettings/taxrates";
-
-export default function TaxRate() {
-  return (
-    <>
-      <TaxRatesComponent />
-    </>
-  );
-}
